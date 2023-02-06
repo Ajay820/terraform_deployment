05314fb59d8e3be718315958d55193d6a10e4016
Ajay <75231308+Ajay820@users.noreply.github.com>
2023-02-06 18:00:24 +0530

    commit for easy understanding
    comments have been added to better understanding the file

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
+# this is for giving the provider name
 provider "aws" {
   region="ap-south-1"
 }
+#this is for creating a vpc
 
 resource "aws_vpc" "custom" {
   cidr_block = "10.0.0.0/16"
 }
+
+#this is for creating a subnet public subnet
 
 resource "aws_subnet" "public" {
   vpc_id = aws_vpc.custom.id
@@ -12,6 +16,7 @@ resource "aws_subnet" "public" {
   cidr_block = "10.0.0.0/24"
 }
 
+#this is for creating a private subnet
 
 resource "aws_subnet" "private" {
   vpc_id = aws_vpc.custom.id
@@ -19,9 +24,13 @@ resource "aws_subnet" "private" {
   map_public_ip_on_launch = true
 }
 
+#this is for creating a internet gateway
+
 resource "aws_internet_gateway" "customigw" {
   vpc_id = aws_vpc.custom.id
 }
+
+#this is for creating route table
 
 resource "aws_route_table" "route" {
     vpc_id = aws_vpc.custom.id
@@ -30,6 +39,8 @@ resource "aws_route_table" "route" {
         gateway_id = aws_internet_gateway.customigw.id
     }
 }
+
+#this is for creating subnet association
 
 resource "aws_route_table_association" "subnet-association" {
   route_table_id = aws_route_table.route.id
